37f51d82c141df068b3e35b10da20a0bc1dbe3f4
selfjell <h.trondsen@hotmail.com>
2021-02-25 16:32:54 +0100

    cleanup, add comments

--- aligner.py
+++ aligner.py
@@ -25,9 +25,6 @@ def read_score_matrix(path):
             #remove letter
             lst = lst[1:]
             scores.append([int(x) for x in lst if x != "" and x != "\n"])
-    assert len(indexes) == 24
-    assert len(scores) == 24
-    assert len(scores[0]) == 24
     return indexes, scores
 
 def format_and_print(a,b,x, a_start, b_start, a_end, b_end, width = 50):
@@ -80,15 +77,15 @@ seq2 = seq2.replace("\n","")
 indexes, scores = read_score_matrix(sys.argv[3])
 gap_opening = 11
 gap_extention = 1
-verbose = False
+debug = False
 #parse optional arguments
 for arg in sys.argv:
     if arg.startswith("go"):
         gap_opening = int(arg.split("=")[1])
     elif arg.startswith("ge"):
         gap_extention = int(arg.split("=")[1])
-    elif arg.startswith("v"):
-        verbose = True
+    elif arg.startswith("d"):
+        debug = True
 print(f'Penalty: Affine\nGap-opening penalty: {gap_opening}\nGap-extention penalty: {gap_extention}')
 print(f'Score matrix:{sys.argv[3]}')
 
@@ -111,8 +108,8 @@ highest_score = [0,0,0]
 
 for i in range(1,len(seq1)+1):
     for j in range(1,len(seq2)+1):
-        temp = matrix[i-1][j-1] + scores[indexes[seq1[i-1]]][indexes[seq2[j-1]]]
-        #I don't include gap-extention penalty for the initial gaps
+        diag = matrix[i-1][j-1] + scores[indexes[seq1[i-1]]][indexes[seq2[j-1]]]
+        #I purposefully don't include gap-extention penalties for the initial gaps
         F[i][j] = max(F[i-1][j]-gap_extention,matrix[i-1][j]-gap_opening,0)
         E[i][j] = max(E[i][j-1]-gap_extention,matrix[i][j-1]-gap_opening,0) 
         #Assuming no insertion right after deletions
@@ -120,14 +117,14 @@ for i in range(1,len(seq1)+1):
             F_dir[i][j] = "U"
         elif F[i][j] == matrix[i-1][j]-gap_opening:
             F_dir[i][j] = "D"
-        #F_dir[i][j] = "U" if F[i][j] == F[i-1][j]-gap_extention else "D"
+
         if E[i][j] == E[i][j-1]-gap_extention:
             E_dir[i][j] = "L"  
         elif E[i][j] == matrix[i][j-1]-gap_opening:
             E_dir[i][j] = "D"
 
-        matrix[i][j] = max(temp, F[i][j],E[i][j],0)
-        if matrix[i][j] == temp:
+        matrix[i][j] = max(diag, F[i][j],E[i][j],0)
+        if matrix[i][j] == diag:
             dir_matrix[i][j] = "D"        
         elif matrix[i][j] == F[i][j]:
             dir_matrix[i][j] = "U"           
@@ -143,13 +140,14 @@ for i in range(1,len(seq1)+1):
 align1, align2, alignX = '','',''
 i,j = highest_score[0],highest_score[1]
 
-#trim arrays
+#trim first row and column of arrays
 E = [row[1:] for row in E[1:]]
 E_dir = [row[1:] for row in E_dir[1:]]
 F = [row[1:] for row in F[1:]]
 F_dir = [row[1:] for row in F_dir[1:]]
 
-if verbose:
+#print all the matrices for debugging
+if debug:
     print("Score matrix (main)")
     for row in matrix:
         print(row)
@@ -177,7 +175,8 @@ cur_dir = dir_matrix
 score_matrix = matrix
 #find the alignment using direction matrix, stops when it hits a zero
 while i >= 0 or j >= 0:
-    print(f'I: {i} J: {j}')
+    if debug:
+        print(f'I: {i} J: {j}')
     if score_matrix[i][j] <= 0:
         #terminate
         seq2_start = prev_j
@@ -185,6 +184,7 @@ while i >= 0 or j >= 0:
         break
     dir = cur_dir[i][j]
     prev_i, prev_j = i, j
+    #move based on which matrix we are in
     if cur_dir == dir_matrix:
         i -= 1
         j -= 1
@@ -192,25 +192,30 @@ while i >= 0 or j >= 0:
         j-=1
     elif cur_dir == F_dir:
         i-=1
-    print(f'Elements in list: {dir}')
-    #Keep track of previous in case next one is a zero
-    
+    if debug:
+        print(f'Elements in list: {dir}')
+    #deletion
     if "L" == dir:
-        print(f'Deletion for residue:{seq2[j]}')
+        if debug:
+            print(f'Deletion for residue:{seq2[j]}')
         align1 = "-" + align1
         align2 = seq2[j] + align2
         alignX  = ' ' + alignX
-        next_matrix = E_dir
+        cur_dir = E_dir
         score_matrix = E
+    #insertion
     elif "U" == dir:
-        print(f'Insertion for residue:{seq1[i]}')
+        if debug:
+            print(f'Insertion for residue:{seq1[i]}')
         align1  = seq1[i] + align1
         align2 = "-" + align2
         alignX = " " + alignX
-        next_matrix = F_dir
+        cur_dir = F_dir
         score_matrix = F
+    #match/mismatch
     elif "D" == dir:
-        print(f'Alignment for: {seq1[i]} and {seq2[j]}')
+        if debug:
+            print(f'Alignment for: {seq1[i]} and {seq2[j]}')
         align1 = seq1[i] + align1
         align2 = seq2[j] + align2
         if seq1[i] == seq2[j]:
@@ -219,34 +224,32 @@ while i >= 0 or j >= 0:
             alignX = "+" + alignX
         else:
             alignX =  " " + alignX
-        next_matrix = dir_matrix
+        cur_dir = dir_matrix
         score_matrix = matrix
     
-    
-    cur_dir = next_matrix
-    
-
 
 seq2_end = highest_score[1]
 seq1_end = highest_score[0]
 
-#printing results 
+#print alignment 
 print(f'Sequence A:\n{header_seq1}{seq1}')
 print(f'Sequence B:\n{header_seq2}{seq2}')
 print(f'Score: {highest_score[2]}\nAlignment:')
 format_and_print(align1,align2,alignX, seq1_start, seq2_start, seq1_end, seq2_end, width=50)
 
-actual_score = 0
-inGap = False
-for i in range(len(align1)):
-    if align1[i] == "-" or align2[i] == "-":
-        if inGap:
-            actual_score -= gap_extention
+#Verify the score based on printed alignment
+if debug:
+    actual_score = 0
+    inGap = False
+    for i in range(len(align1)):
+        if align1[i] == "-" or align2[i] == "-":
+            if inGap:
+                actual_score -= gap_extention
+            else:
+                actual_score = (actual_score) - gap_opening
+                inGap = True
         else:
-            actual_score = (actual_score) - gap_opening
-            inGap = True
-    else:
-        inGap = False
-        actual_score += scores[indexes[align1[i]]][indexes[align2[i]]]
-print(f'Actual score from alignment: {actual_score}')
+            inGap = False
+            actual_score += scores[indexes[align1[i]]][indexes[align2[i]]]
+    print(f'Actual score from alignment: {actual_score}')
 
